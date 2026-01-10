83c94b83f6e5260afc6c85afac0f75ecd6adfebc
HarshalPatil <patilharshal2282@gmail.com>
2026-01-10 22:25:25 +0530

    Utils code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        messege="Something went wrong",
+        errors=[],
+        statck =""
+    ){
+        super(messege);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = messege;
+        this.success = false;
+        this.errors = errors;
+        if(statck){
+            this.statck = statck;
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export{ApiError}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// promises
+const asyncHandler= (requestHandler)=>{
+    async(req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+        
+    }
+}
+export {asyncHandler}
+
+
+// const asyncHandler = (func) =>{async()=>{}}
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next) 
+//     } catch (error) {
+//         res.status(error.code ||500).json({
+//             success: false,
+//             messege: err.messege
+//         })
+//     }
+
+// }
